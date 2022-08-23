c863f4d4eb021a8d6923a7d406d8ae35d884b3e3
Artem Rychenkov <fleshmercy@gmail.com>
2022-08-23 23:12:31 +0300

    fix gitignore

---
+++ BackupTool/Backupper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupTool
+{
+    public class Backupper
+    {
+        public static void Backup(string baseDir, string targetDir)
+        {
+            ArgumentNullException.ThrowIfNull(baseDir, nameof(baseDir));
+            ArgumentNullException.ThrowIfNull(targetDir, nameof(targetDir));
+
+            if (baseDir == targetDir || !Directory.Exists(baseDir))
+                return;
+
+            var backupDirPath = Path.Combine(targetDir, Path.GetFileName(baseDir));
+            Directory.CreateDirectory(backupDirPath);
+
+            foreach (var dirPath in Directory.GetDirectories(baseDir, "*", SearchOption.AllDirectories))
+            {
+                Directory.CreateDirectory(dirPath.Replace(baseDir, backupDirPath));
+            }
+
+            foreach (var filePath in Directory.GetFiles(baseDir, "*", SearchOption.AllDirectories))
+            {
+                File.Copy(filePath, filePath.Replace(baseDir, backupDirPath), true);
+            }
+        }
+    }
+}
---
+++ BackupTool/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text.Json.Nodes;
+
+try
+{
+    var json = File.ReadAllText("settings.json");
+    var settingsObj = JsonNode.Parse(json);
+
+    foreach (var dir in settingsObj["base_dirs"].AsArray())
+        BackupTool.Backupper.Backup((string)dir, (string)settingsObj["target_dir"]);
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
