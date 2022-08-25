8af8a81d42652bf3f396910dccbc9ecd1d741cc1
Artem Rychenkov <fleshmercy@gmail.com>
2022-08-25 19:42:58 +0300

    Update logging in Program.cs

--- BackupTool/Program.cs
+++ BackupTool/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.Json.Nodes;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BackupTool;
@@ -19,14 +21,41 @@ var logger = serviceProvider.GetRequiredService<ILogger<Program>>();
 
 try
 {
+    logger.LogInformation("Start BackupTool");
+
     var json = File.ReadAllText("settings.json");
     var settingsObj = JsonNode.Parse(json);
-    
+
     var backupService = serviceProvider.GetService<IBackupService>();
-    foreach (var dir in settingsObj["base_dirs"].AsArray())
-        backupService.Backup((string)dir, (string)settingsObj["target_dir"]);
+    var target_dir = (string)settingsObj["targetDir"];
+
+    if (settingsObj["baseDirs"]?.AsArray() is { } base_dirs)
+    {
+        foreach (var dir in base_dirs)
+            backupService?.Backup((string)dir, target_dir);
+    }
+    else
+    {
+        logger.LogInformation("baseDirs parameter is null, no base directory to copy");
+    }
 }
-catch(Exception ex)
+catch (FileNotFoundException ex)
 {
-    Console.WriteLine(ex.Message);
+    logger.LogError($"Could not find settings file {ex.FileName}");
+}
+catch (JsonException ex)
+{
+    logger.LogError($"Invalid settings json file, error line number: {ex.LineNumber}");
+}
+catch (ArgumentNullException ex)
+{
+    logger.LogError($"Invalid value in settings, directory {ex.ParamName} is null");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex);
+}
+finally
+{
+    logger.LogInformation("BackupTool stoped");
 }
